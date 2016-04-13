c1315837b5a3cae5bdbed5170bda65817402aef3
tswales <tom.g.swales@gmail.com>
2016-04-12 21:47:36 -0400

    First commit

---
+++ SportsPool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportsPool.MvcApplication" Language="C#" %>
---
+++ SportsPool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SportsPool.Startup))]
+namespace SportsPool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
